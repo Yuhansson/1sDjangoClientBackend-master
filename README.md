0575e8b4a2acb8386ca1dd612fa7fbd8257b0139
Yuhasa <yuhasa@yandex.ru>
2022-02-10 18:33:10 +0300

    Копия 11111

---
+++ README.md
@@ -0,0 +1,1 @@
+# 1sDjangoClientLoc Личный кабинет клиента 1c (Python+Django)
---
+++ lk/users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from .views import *
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'api/v1/users', CustomUserViewSet, basename='users')
+router.register(r'api/v1/user', ProfileCustomUserViewSet, basename='user')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api/v1/user_logout/', LogoutView.as_view(), name='user_logout')
+]
+
